c5ead70fd80e2ccf61d87d253a465ac6ee89a6a1
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-08-13 13:10:03 -0300

    Criando adapter para user admin

---
+++ playerstars_adapters/user_admin_adapter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from playerstars_adapters import BasicDynamodbAdapter
+from playerstars_domain import UserAdmin
+
+
+class UserAdminAdapter(BasicDynamodbAdapter):
+    __table_name__ = 'UserAdmin'
+
+    def __init__(self):
+        super(UserAdminAdapter, self).__init__(self.__table_name__, UserAdmin)

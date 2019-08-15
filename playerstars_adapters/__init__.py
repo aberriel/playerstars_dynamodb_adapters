f79effeda346ca2ef26559a25b9d860e21745641
Anselmo Lira <anselmo.lira@stormsec.com.br>
2019-08-15 13:35:30 -0300

    Adicionando o adapter para os times

--- playerstars_adapters/__init__.py
+++ playerstars_adapters/__init__.py
@@ -12,4 +12,5 @@ from .user_adapter import UserAdapter
 from .player_adapter import PlayerAdapter
 from .region_country_adapter import CountryRegionAdapter
 from .region_state_adapter import StateRegionAdapter
+from .team_adapter import TeamAdapter
 from .user_admin_adapter import UserAdminAdapter

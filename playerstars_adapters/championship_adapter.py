204cab4425c1f7de4aeafb74088ec61109160ce7
Anselmo Lira <anselmo.lira@stormsec.com.br>
2019-10-16 17:10:31 -0300

    Alterações neste commit:
    1 - Ajustando o ChampionshipAdapter para ser aprovado no linter, pois surgiu o problema de linha longa para a linha 7
    (resulvido quebrando a linha).
    
    2 - Resolvendo problemas nos testes de PlayerAdapter que surgiram por ponta de ajustes que fiz no domain:
    	-> datebirth do User, agora, é um DateField e não mais um DateTimeField (no teste, esperava hora, minuto e
    segundo)
    	-> Adicionando favorites, state_regions, countries_regions e points no player_dict_from_db para assegurar que o
    test_player_get passe.

--- playerstars_adapters/championship_adapter.py
+++ playerstars_adapters/championship_adapter.py
@@ -4,4 +4,6 @@ from playerstars_domain import Championship
 
 class ChampionshipAdapter(BasicDynamodbAdapter):
     def __init__(self, table_name, db_endpoint):
-        super(ChampionshipAdapter, self).__init__(table_name, db_endpoint, Championship)
+        super(ChampionshipAdapter, self).__init__(table_name,
+                                                  db_endpoint,
+                                                  Championship)

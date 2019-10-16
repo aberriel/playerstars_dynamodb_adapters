462b4561ddda56743901139d8928430bbae7ad4c
Anselmo Lira <anselmo.lira@stormsec.com.br>
2019-10-16 16:58:51 -0300

    Implementação do ChampionshipAdapter e sua inclusão no playerstars_dynamo_adapters.__init__

---
+++ playerstars_adapters/championship_adapter.py
@@ -0,0 +1,7 @@
+from playerstars_adapters import BasicDynamodbAdapter
+from playerstars_domain import Championship
+
+
+class ChampionshipAdapter(BasicDynamodbAdapter):
+    def __init__(self, table_name, db_endpoint):
+        super(ChampionshipAdapter, self).__init__(table_name, db_endpoint, Championship)

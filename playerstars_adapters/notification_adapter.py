a41946320c6406e1315b3e8f6990fd32a3f68dd7
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-10-30 11:14:58 -0300

    Criando adapter de notificação

---
+++ playerstars_adapters/notification_adapter.py
@@ -0,0 +1,7 @@
+from playerstars_adapters import BasicDynamodbAdapter
+from playerstars_domain import Notification
+
+
+class NotificationAdapter(BasicDynamodbAdapter):
+    def __init__(self, table_name, db_endpoint):
+        super(NotificationAdapter, self).__init__(table_name, db_endpoint, Notification)

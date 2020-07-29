bbe2c72a440dc1b2f99a114f8bfb2005f65bdfeb
Marcos Lima <marcos.j.s.lima@gmail.com>
2020-07-29 14:22:39 -0300

    Upgrade clapy-dynamodb-adapter 0.2.2 -> 0.3.0

--- requirements.py
+++ requirements.py
@@ -1,8 +1,8 @@
 requirements = [
     'playerstars-domain',
-    'boto3==1.9.190',
+    'boto3==1.12.42',
     'clapy-basic-classes==0.3.0',
-    'clapy-dynamodb-adapter==0.2.2',
+    'clapy-dynamodb-adapter==0.3.0',
 ]
 
 setup_requirements = []

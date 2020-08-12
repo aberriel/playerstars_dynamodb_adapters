bc892baedd9d9a7fe7383ee81ea8868e3fcf7e5a
Marcos Lima <marcos.j.s.lima@gmail.com>
2020-08-12 11:41:54 -0300

    Implementada classe EventReminderAssistantAdapter

---
+++ tests/test_event_reminder_assistant_adapter.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from playerstars_domain.event_reminder_assistant import EventReminderAssistant
+
+from playerstars_adapters.event_reminder_assistant_adapter import \
+    EventReminderAssistantAdapter
+
+
+@patch('clapy_dynamodb_adapter.basic_dynamodb_adapter'
+       '.BasicDynamodbAdapter.__init__')
+def test_era_adapter(mock_init):
+    EventReminderAssistantAdapter(table_name='era_table')
+
+    mock_init.assert_called_with(table_name='era_table',
+                                 db_endpoint=None,
+                                 adapted_class=EventReminderAssistant)
